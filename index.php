ee64108602a2ed76cdaa90e6a8b4fb5c4d3c933d
Aakash Kathunia <aakashkathunia@gmail.com>
2020-08-11 02:23:09 +0530

    second commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "Linux World!!!";
+echo "### DevOpsTask3 ###";
+echo "!!! Jenkins With Kubernetes !!!";
+echo "Redhat";
+echo "Git";
+echo "Jenkins";
+echo "Kubernetes";
+?>
+
+</body>
+</html> 
